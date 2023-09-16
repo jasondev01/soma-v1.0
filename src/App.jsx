cbea579f96d995d186af41afde4ca41c6e2dc6fb
jasondev01 <jsnrbn01@gmail.com>
2023-09-16 22:42:16 +0800

    maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -16,36 +16,67 @@ import LoginPage from "./pages/LoginPage"
 import AuthLayout from "./layouts/AuthLayout"
 import RegisterPage from "./pages/RegisterPage"
 import SettingPage from "./pages/SettingPage"
+import Maintenance from "./pages/Maintenance"
+import useAuthContext from "./context/AuthContext"
+import { useEffect, useState } from "react"
+import { admin } from "./utilities/service"
 // import { Analytics } from '@vercel/analytics/react';
 
 function App() {
+    const [ isAdmin, setIsAdmin ] = useState(false)
+    const { user } = useAuthContext();
+
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem('User'))
+        if (storedUser) {
+            if(storedUser?.email === admin || user?.email === admin) {
+                setIsAdmin(true)
+            } else {
+                setIsAdmin(false)
+            }
+        } else {
+            setIsAdmin(false)
+        }
+    }, [])
 
     return (
         <>
             {/* <Analytics mode={'production'} /> */}
             <Router>
-                <Routes>
-                    <Route element={<MainLayout />} > 
-                        <Route path="/" element={<Home />} />
-                        <Route path="/dmca" element={<DMCA />} />
-                        <Route path="/terms" element={<Terms />} />
-                        <Route path="/search/:query" element={<Search />} />
-                        <Route path="/latest" element={<LatestPage />} />
-                        <Route path="/popular" element={<PopularPage />} />
-                        <Route path="/new-season" element={<NewSeason />} />
-                        <Route path="/info/:id" element={<Info />} />
-                        <Route path="/watch/:id/:episode/:episodeId" element={<Watch />} />
-                        <Route path="/pass/:id/:epNum" element={<PassingData />} />
+                {
+                    isAdmin ? (
+                        <Routes>
+                            <Route element={<MainLayout />} > 
+                                <Route path="/" element={<Home />} />
+                                <Route path="/dmca" element={<DMCA />} />
+                                <Route path="/terms" element={<Terms />} />
+                                <Route path="/search/:query" element={<Search />} />
+                                <Route path="/latest" element={<LatestPage />} />
+                                <Route path="/popular" element={<PopularPage />} />
+                                <Route path="/new-season" element={<NewSeason />} />
+                                <Route path="/info/:id" element={<Info />} />
+                                <Route path="/watch/:id/:episode/:episodeId" element={<Watch />} />
+                                <Route path="/pass/:id/:epNum" element={<PassingData />} />
 
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/edit-profile" element={<SettingPage />} />
-                        <Route path='*' element={<Navigate to='/' />} />
-                    </Route>
-                    <Route element={<AuthLayout />} >
-                        <Route path="/login" element={ <LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                    </Route>
-                </Routes>
+                                <Route path="/profile" element={<Profile />} />
+                                <Route path="/edit-profile" element={<SettingPage />} />
+                                <Route path='*' element={<Navigate to='/' />} />
+                            </Route>
+                            <Route element={<AuthLayout />} >
+                                <Route path="/login" element={ <LoginPage />} />
+                                <Route path="/register" element={<RegisterPage />} />
+                            </Route>
+                        </Routes>
+                    ) : (
+                        <Routes>
+                            <Route element={<AuthLayout />} >
+                                <Route path="/" element={ <Maintenance />} />
+                                <Route path="/login" element={ <LoginPage />} />
+                                <Route path="/register" element={<RegisterPage />} />
+                            </Route>
+                        </Routes>
+                    )
+                }
             </Router>
         </>
     )
---
+++ src/pages/Maintenance.jsx
@@ -0,0 +1,18 @@
+
+const Maintenance = () => {
+
+    return (
+        <div className="container">
+            <p style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+            }}>
+                Apologies but soma is currently undergoing a maintenance. We'll be back soon!
+            </p>
+        </div>
+    )
+}
+
+export default Maintenance
